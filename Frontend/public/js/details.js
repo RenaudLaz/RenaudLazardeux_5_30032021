ac20d3153535a6197f3443ed82bf9ad53adfb7e4
Renaud Lazardeux <Renaud.lazardeux@gmail.com>
2021-08-21 13:49:43 +0200

    premiere version details.js

--- Frontend/public/js/details.js
+++ Frontend/public/js/details.js
@@ -4,6 +4,65 @@ fetch(apiProductListUrl)
     .then(data => 
         {
             console.log(data);
-    
+            recupTitre(data);
+            recupPhoto(data);
+            recupDescription(data);
+            recupCouleur(data);
+            recupPrix(data);
         }   
     )
+
+    function recupTitre(data)
+    {
+        let i=0
+        console.log(data[i].name);
+
+        titre = document.createElement('h1');
+        titre.innerHTML +=  data[i].name;
+
+        document.getElementById('detail_teddy').appendChild(titre);
+    }
+
+    function recupPhoto(data)
+    {
+        let i=0
+        console.log(data[i].imageUrl);
+
+        image = document.createElement('img');
+        image.src +=  data[i].imageUrl;
+
+        document.getElementById('detail_teddy').appendChild(image);
+    }
+
+    function recupDescription(data)
+    {
+        let i=0
+        console.log(data[i].description);
+
+        description = document.createElement('p');
+        description.innerHTML +=  data[i].description;
+
+        document.getElementById('detail_teddy').appendChild(description);
+    }
+
+    function recupCouleur(data)
+    {
+        let i=0
+        console.log(data[i].colors);
+
+        couleur = document.createElement('li');
+        couleur.innerHTML +=  data[i].colors;
+
+        document.getElementById('detail_teddy').appendChild(couleur);
+    }
+
+    function recupPrix(data)
+    {
+        let i=0
+        console.log(data[i].price);
+
+        prix = document.createElement('div');
+        prix.innerHTML +=  '<p class="price">' + (data[i].price / 100) + '€' + '</p>';
+
+        document.getElementById('detail_teddy').appendChild(prix);
+    }
